9ff03b31f7d126fc9fa877f0eb7563dc8ddf4ccc
Richard Stuart <richard.stuart91@gmail.com>
2021-09-07 22:43:19 +0200

    motor and LED commands

--- PlantyLib.py
+++ PlantyLib.py
@@ -8,6 +8,7 @@ from time import sleep
 from enum import Enum
 
 class PlantyConnect:
+	#Port to access Arduino. Baudrate 57600 default. Delay in ms between send and recieved messages
 	def __init__(self, port, baudrate, delay):
 		if port == "":
 			try:
@@ -27,7 +28,8 @@ class PlantyConnect:
 			print(str(SerialEx))
 			
 		sleep(2)
-			
+	
+	#Get connected port from arduino if no port has been specified		
 	def __getPort(self):
 		portlist = serial.tools.list_ports.comports(include_links=False)
 		arduinoport = ""
@@ -41,15 +43,18 @@ class PlantyConnect:
 			
 		return arduinoport
 	
+	#Close port
 	def closePort(self):
 		self.ser.flush()
 		self.ser.close()
-		
+	
+	#Send message
 	def write(self, message):
 		message = message + '\n'
 		self.ser.write(message.encode('utf-8'))
 		sleep(self.delay)
-		
+	
+	#Read incoming message
 	def read(self):	
 		if self.ser.in_waiting > 0:
 			while self.ser.in_waiting > 0:
@@ -57,15 +62,24 @@ class PlantyConnect:
 			return rec
 		else:
 			raise Exception("No incoming message")
-			
+	
+	#Set delay between send and recieve
 	def setDelay(self,newDelay):
 		self.delay = newDelay
 
 class TempOption(Enum):
 	TEMP = 1
 	HUMIDITY = 2
+	
+class ColorOption(Enum):
+	PURPLE = 1
+	WHITE = 2
+	RED = 3
+	GREEN = 4
+	BLUE = 5
 			
 class PlantyCommands:
+	#Port to access Arduino. Baudrate 57600 default. Delay in ms between send and recieve
 	def __init__(self, port, baudrate, delay):
 		self.connect = PlantyConnect(port,baudrate,delay)
 		self.recMessage = ""
@@ -99,12 +113,14 @@ class PlantyCommands:
 			self.recMessage = self.__getCommandValue(rec)
 		else:
 			raise Exception("Error when recieving message: " + str(rec))
-		
+	
+	#Read plant name from NVM	
 	def readPlant(self):
 		self.sendMessage = "PLANT=1"
 		self.__sendMessage()
 		self.__recMessage()
-		
+	
+	#Write plant name to NVM	
 	def writePlant(self, plant):
 		oldDelay = self.connect.delay
 		self.connect.setDelay(0.5)
@@ -112,7 +128,8 @@ class PlantyCommands:
 		self.__sendMessage()
 		self.__recMessage()
 		self.connect.setDelay(oldDelay)
-		
+	
+	#Read temperature or humidity
 	def readTemp(self, TempOption):
 		if TempOption == TempOption.TEMP:
 			self.sendMessage = "TEMP=1"
@@ -122,7 +139,8 @@ class PlantyCommands:
 			self.sendMessage = "TEMP=2"
 			self.__sendMessage()
 			self.__recMessage()
-			
+	
+	#Read moisture sensor. Average value from n samples		
 	def readMoisture(self, samples):
 		oldDelay = self.connect.delay
 		newDelay = oldDelay + (samples*400)/1000
@@ -131,9 +149,25 @@ class PlantyCommands:
 		self.__sendMessage()
 		self.__recMessage()
 		self.connect.setDelay(oldDelay)
-		
+	
+	#Read light sensor
 	def readALS(self):
 		self.sendMessage = "ALS"
 		self.__sendMessage()
 		self.__recMessage()
+	
+	#Start or stop motor. Duration [ms]
+	def setMotor(self, start, power, duration):
+		if start:
+			self.sendMessage = "MOTR=1," + str(power) + "," + str(duration)
+		else:
+			self.sendMessage = "MOTR=0," + str(power) + "," + str(duration)
+	
+	#Set LED
+	def setLight(write, ColorOption, power):
+		if write:
+			self.sendMessage = "LED=1," + str(ColorOption) + "," + str(power)
+		else:
+			self.sendMessage = "LED=2"
+			
 					
